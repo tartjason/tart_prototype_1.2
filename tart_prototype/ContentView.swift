f305b0242a9ba1c6eb257d6de87b6a1fc53c4018
tartjason <jason.softart@gmail.com>
2025-04-28 21:33:04 +0800

    Initial Commit

---
+++ tart_prototype/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tart_prototype
+//
+//  Created by ZhengXian Lin on 4/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
